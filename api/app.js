0698f49df545deaf99339594b47fe6dcd7c88bea
DaZiYuan <mscoder@hotmail.com>
2020-01-13 15:42:46 +0800

    增加cookies测试代码

--- api/app.js
+++ api/app.js
@@ -11,4 +11,10 @@ app.get('/', (req, res) => {
   res.send(JSON.stringify({ message: `Server time: ${new Date().toString()}` }))
 })
 
+app.post("/testCookie", (req, res, next) => {
+  res.cookie("test0", '000');
+  res.cookie("test1", 111);
+  res.send('Cookie is set');
+});
+
 module.exports = app
